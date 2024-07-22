83ef2ef64a91482e63f07d21aced2faec445f077
Ahsan Tahir <atahir@visp.net>
2024-07-22 12:16:53 +0500

    [CRM-315] UpdateTicket Mutation > Response fields seems inappropriate in 'updateTicket' mutation when update ticket description

--- src/TicketDetails/TicketGraphQL.js
+++ src/TicketDetails/TicketGraphQL.js
@@ -74,13 +74,6 @@ export const UPDATE_TICKET_MUTATION = gql`
   mutation UpdateTicket($input_ticket: TicketInput!) {
     updateTicket(input_ticket: $input_ticket) {
       ticket_id
-      priority
-      status
-      type
-      due_by_date
-      earliest_arrival_time
-      latest_arrival_time
-      address
     }
   }
 `;
