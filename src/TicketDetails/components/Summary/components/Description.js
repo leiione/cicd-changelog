0c7dd91c63bb238c8104646f99432e5ee7b4f2df
Ahsan Tahir <atahir@visp.net>
2024-07-22 14:04:40 +0500

    [CRM-312] Update Description > 'Save' and 'Cancel' should automatically hide once description updated successfully

--- src/TicketDetails/components/Summary/components/Description.js
+++ src/TicketDetails/components/Summary/components/Description.js
@@ -69,6 +69,7 @@ const Description = (props) => {
                             size="small"
                             onClick={handleCancel}
                             className="my-1"
+                            style={{ opacity: loading ? 0.5 : 1 }} // Adjust opacity to make button look active
                             disabled={loading}
                         >
                             Cancel
