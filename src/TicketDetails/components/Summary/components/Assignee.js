009e8ad1c5212ecc40467444762b6029b9a0c523
saloni47b <saloni.mantri@47billion.com>
2024-07-24 09:53:52 +0530

    Update Assignee.js

--- src/TicketDetails/components/Summary/components/Assignee.js
+++ src/TicketDetails/components/Summary/components/Assignee.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import {
   Grid,
   Typography,
-  MenuItem,
   Popover,
   Avatar,
   Button,
+  ListItem,
+  List,
+  ListItemIcon,
+  ListItemText,
 } from "@mui/material";
 import { preventEvent } from "Common/helper";
 import { useQuery } from "@apollo/client";
@@ -43,33 +46,33 @@ const Assignee = (props) => {
   const { data } = useQuery(GET_ASSIGNEES, {
     fetchPolicy: "network-only",
   });
-  
+
   function stringToColor(string) {
     let hash = 0;
     let i;
-  
+
     /* eslint-disable no-bitwise */
     for (i = 0; i < string.length; i += 1) {
       hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
-  
-    let color = '#';
-  
+
+    let color = "#";
+
     for (i = 0; i < 3; i += 1) {
       const value = (hash >> (i * 8)) & 0xff;
       color += `00${value.toString(16)}`.slice(-2);
     }
     /* eslint-enable no-bitwise */
-  
+
     return color;
   }
-  
+
   function stringAvatar(name) {
     return {
       sx: {
         bgcolor: stringToColor(name),
       },
-      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
     };
   }
   return (
@@ -108,21 +111,25 @@ const Assignee = (props) => {
               horizontal: "left",
             }}
           >
-            {data &&
-              data.assignees &&
-              data.assignees.map((assignee) => (
-                <MenuItem
-                  key={assignee.appuser_id} // Use a unique identifier as key
-                  onClick={(event) => handlePopoverClose(event, assignee)} // Adjust this handler as needed
-                >
-                  <Avatar
-                    {...stringAvatar(assignee.realname)}
-                    sx={{ width: 24, height: 24 }}
-                    className="mr-2"
-                  />
-                  {assignee.realname}
-                </MenuItem>
-              ))}
+            <List>
+              {data &&
+                data.assignees &&
+                data.assignees.map((assignee) => (
+                  <ListItem
+                    key={assignee.appuser_id} // Use a unique identifier as key
+                    onClick={(event) => handlePopoverClose(event, assignee)} // Adjust this handler as needed
+                  >
+                    <ListItemIcon>
+                      <Avatar
+                        {...stringAvatar(assignee.realname)}
+                        sx={{ width: 24, height: 24 }}
+                      />
+                    </ListItemIcon>
+
+                    <ListItemText primary={assignee.realname} />
+                  </ListItem>
+                ))}
+            </List>
             <div className="drawer-footer">
               <Button color="primary" variant="outlined">
                 Save
