0e0bd65b65943bc9305a4c73d31c9f41ab648e66
Ahsan Tahir <atahir@visp.net>
2024-07-29 16:09:39 +0500

    [CRM-360] 'Save' and 'Cancel' buttons should be enabled only if there is change

--- src/TicketDetails/components/Summary/components/WorkOrder.js
+++ src/TicketDetails/components/Summary/components/WorkOrder.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { Editor } from "@tinymce/tinymce-react";
 import { GET_DETAIL_TEXT, UPDATE_DETAIL_TEXT } from "TicketDetails/TicketGraphQL";
@@ -7,17 +7,18 @@ import { useDispatch } from "react-redux";
 import { showSnackbar } from "config/store";
 
 const WorkOrder = ({ ticket_id }) => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [initialDetailText, setInitialDetailText] = useState("");
     const [detailText, setDetailText] = useState("");
     const [ticket_type_id, setTicketTypeID] = useState("");
     const [detail_id, setDetailID] = useState("");
-    const [isEditing, setIsEditing] = useState(false);
 
     const { loading } = useQuery(GET_DETAIL_TEXT, {
         variables: { ticket_id },
         fetchPolicy: "network-only",
         onCompleted: (data) => {
             if (data && data.workflowOrder) {
+                setInitialDetailText(data.workflowOrder.detail_text);
                 setDetailText(data.workflowOrder.detail_text);
                 setDetailID(data.workflowOrder.detail_id);
                 setTicketTypeID(data.workflowOrder.ticket_type_id);
@@ -44,10 +45,10 @@ const WorkOrder = ({ ticket_id }) => {
                 },
             });
             if (data.updateTicket.ticket_id) {
-                dispatch(showSnackbar({ message: "The work order saved successfully", severity: "success" }))
-                setIsEditing(false);
+                dispatch(showSnackbar({ message: "The work order saved successfully", severity: "success" }));
+                setInitialDetailText(detailText);
             } else {
-                dispatch(showSnackbar({ message: "Failed to save work order.", severity: "error" }))
+                dispatch(showSnackbar({ message: "Failed to save work order.", severity: "error" }));
             }
         } catch (error) {
             console.error("There was an error updating the detail text!", error);
@@ -55,7 +56,7 @@ const WorkOrder = ({ ticket_id }) => {
     };
 
     const handleCancel = () => {
-        setIsEditing(false);
+        setDetailText(initialDetailText);
     };
 
     const handlePrint = () => {
@@ -100,53 +101,49 @@ const WorkOrder = ({ ticket_id }) => {
 
     return (
         <div>
-            {isEditing ? (
-                <>
-                    <Editor
-                        apiKey="rv98fsqigjw4pj7zsbawye8jrdpgxrrhzznj01jou3tgj7ti" // replace with your TinyMCE API key
-                        value={detailText}
-                        init={{
-                            height: 500,
-                            menubar: false,
-                            plugins: [
-                                'advlist autolink lists link image charmap print preview anchor',
-                                'searchreplace visualblocks code fullscreen',
-                                'insertdatetime media table paste code help wordcount image',
-                                'file'
-                            ],
-                            toolbar:
-                                'undo redo | formatselect | bold italic backcolor | \
-                                alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help | image | file',
-                            file_picker_callback: handleFilePicker,
-                        }}
-                        onEditorChange={handleEditorChange}
-                    />
-                    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
-                        <Button color="primary" variant="outlined" onClick={handleSave} style={{ marginRight: '10px' }}>
-                            Save
-                        </Button>
-                        <Button variant="outlined" color="default" onClick={handleCancel} style={{ marginRight: '10px' }}>
-                            Cancel
-                        </Button>
-                        <Button variant="contained" onClick={handlePrint}>
-                            Print
-                        </Button>
-                    </div>
-                </>
-            ) : (
-                <Paper
-                    elevation={3}
-                    style={{ padding: '16px', marginTop: '16px', cursor: 'pointer' }}
-                    onClick={() => setIsEditing(true)}
+            <Editor
+                apiKey="rv98fsqigjw4pj7zsbawye8jrdpgxrrhzznj01jou3tgj7ti" // replace with your TinyMCE API key
+                value={detailText}
+                init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen',
+                        'insertdatetime media table paste code help wordcount image',
+                        'file'
+                    ],
+                    toolbar:
+                        'undo redo | formatselect | bold italic backcolor | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | help | image | file',
+                    file_picker_callback: handleFilePicker,
+                }}
+                onEditorChange={handleEditorChange}
+            />
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
+                <Button
+                    color="primary"
+                    variant="outlined"
+                    onClick={handleSave}
+                    style={{ marginRight: '10px' }}
+                    disabled={detailText === initialDetailText}
                 >
-                    <Typography
-                        variant="body1"
-                        dangerouslySetInnerHTML={{ __html: detailText }}
-                        style={{ whiteSpace: 'pre-line' }}
-                    />
-                </Paper>
-            )}
+                    Save
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="default"
+                    onClick={handleCancel}
+                    style={{ marginRight: '10px' }}
+                    disabled={detailText === initialDetailText}
+                >
+                    Cancel
+                </Button>
+                <Button variant="contained" onClick={handlePrint}>
+                    Print
+                </Button>
+            </div>
         </div>
     );
 };
