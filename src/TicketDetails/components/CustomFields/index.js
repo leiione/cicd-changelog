93ba1d181399f496caf550bc9dae8fa494c8e65c
Zain ul Abideen <zabideen@visp.net>
2024-09-02 14:27:33 +0500

    UIUX-1471 - Custom Fields UI
    UIUX-1471 - Custom Fields UI

--- src/TicketDetails/components/CustomFields/index.js
+++ src/TicketDetails/components/CustomFields/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import AccordionCard from "../../../Common/AccordionCard";
 import HeaderMenuOptions from "../../../components/HeaderMenuOptions";
-import {
-  Typography,
-} from "@mui/material";
+import { TextField, Grid } from "@mui/material";
 
 const CustomFields = (props) => {
-    const {appuser_id}= props
+  const { appuser_id } = props;
   return (
     <AccordionCard
       label="Custom Fields"
@@ -17,7 +15,80 @@ const CustomFields = (props) => {
         />
       }
     >
-      <Typography className="text-muted">Custom Fields</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Setup Date"
+            variant="standard"
+            value="07/31/2013"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="O/S"
+            variant="standard"
+            value="Unix"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Previous ISP"
+            variant="standard"
+            value="hellos"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Location ID"
+            variant="standard"
+            value="#568540"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Setup Date"
+            variant="standard"
+            value="07/31/2013"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Previous ISP"
+            variant="standard"
+            value="hellos"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Location ID"
+            variant="standard"
+            value="#568540"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Setup Date"
+            variant="standard"
+            value="07/31/2013"
+          />
+        </Grid>
+      </Grid>
     </AccordionCard>
   );
 };
