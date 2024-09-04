1bae585a74c7497171ea1b1467de284d0ed2f87e
Ahsan Tahir <atahir@visp.net>
2024-09-05 02:29:55 +0500

    [CRM-437]: Delete SMS from Message table

--- src/TicketDetails/components/Messages/components/MessagesTable/index.js
+++ src/TicketDetails/components/Messages/components/MessagesTable/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import TablePagination from "@mui/material/TablePagination";
 import { List, Typography } from "@mui/material";
-
+import { useMutation } from "@apollo/client";
+import { UPDATE_MESSAGE_MUTATION, GET_TICKET_MESSAGES } from "TicketDetails/TicketGraphQL";
 import Note from "./components/Note";
 import Email from "./components/Email";
 import SMS from "./components/SMS";
 import ErrorPage from "components/ErrorPage";
+import { useDispatch } from "react-redux";
+import { showSnackbar } from "config/store";
 
 const MessagesTable = (props) => {
   const { messages, error } = props;
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const dispatch = useDispatch()
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -19,6 +23,31 @@ const MessagesTable = (props) => {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
+  };
+
+  const [updateMessage] = useMutation(UPDATE_MESSAGE_MUTATION)
+
+  const onDeleteMessage = async (messageId, ticketId) => {
+    try {
+      await updateMessage({
+        variables: {
+          input_message: { id: messageId },
+        },
+        refetchQueries: [
+          { query: GET_TICKET_MESSAGES, variables: { ticket_id: ticketId } },
+        ],
+      }).finally(() => {
+        dispatch(showSnackbar({ message: "Message deleted successfully", severity: "success" }))
+      });
+    } catch (error) {
+      const errorMessage = error.message.replace("GraphQL error: ", "");
+      dispatch(
+        showSnackbar({
+          message: errorMessage,
+          severity: "error",
+        })
+      );
+    }
   };
 
   if (error) return <ErrorPage error={error} />;
@@ -34,7 +63,7 @@ const MessagesTable = (props) => {
             case 1:
               return <Email message={message} />
             case 3:
-              return <SMS message={message} />
+              return <SMS message={message} onDeleteMessage={() => onDeleteMessage(message.id, message.ticket_id)} />
             default:
               return null // via FB, etc.
           }
