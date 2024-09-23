69c247ea2ac04c01cb2369df0292b2139d7f14fe
mdelavina <125253593+mdelavina@users.noreply.github.com>
2024-09-23 10:03:36 +0800

    BIL-4803: Queued Job Search Radius Setting

--- src/TicketDetails/components/Summary/components/QueueJobs/components/SearchRadius/index.js
+++ src/TicketDetails/components/Summary/components/QueueJobs/components/SearchRadius/index.js
@@ -8,8 +8,16 @@ const SearchRadius = (props) => {
         { value: 1, label: "1 mile" },
         { value: 5, label: "5 miles" },
         { value: 10, label: "10 miles" },
+        { value: 15, label: "15 miles" },
+        { value: 20, label: "20 miles" },
         { value: 25, label: "25 miles" },
+        { value: 30, label: "30 miles" },
+        { value: 35, label: "35 miles" },
+        { value: 40, label: "40 miles" },
+        { value: 45, label: "45 miles" },
         { value: 50, label: "50 miles" },
+        { value: 55, label: "55 miles" },
+        { value: 60, label: "60 miles" },
     ]
         
     return (
--- src/TicketDetails/components/Summary/components/QueueJobs/components/TicketQueueTable/index.js
+++ src/TicketDetails/components/Summary/components/QueueJobs/components/TicketQueueTable/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { useQuery } from "@apollo/client";
+import { checkIfCacheExists } from "config/apollo";
 import { queueTicketColumns } from "../../../TicketConstants"
+import { GET_QUEUED_JOBS } from "TicketDetails/TicketGraphQL";
 
-const TicketQueueTable = () => {    
+const TicketQueueTable = (props) => {
+    const { custId, radiusMile } = props
+    const {
+        loading,
+        error,
+        data,
+        client,
+      } = useQuery(GET_QUEUED_JOBS, {
+        variables: { custId, radiusMile },
+        fetchPolicy: "cache-and-network"
+      });
+      const cacheExists = checkIfCacheExists(client, {
+        query: GET_QUEUED_JOBS,
+        variables: { custId, radiusMile },
+      });
+
+      let getQueuedJobs = []
+      if (!loading && !error && cacheExists) {
+        getQueuedJobs = data;
+      }
     return (
         <div>
             <DataGrid
                 sx={{ height: 400, maxHeight: 400 }}
                 density="compact"
                 rowHeight={30}
-                rows={[]}
+                rows={getQueuedJobs}
                 columns={queueTicketColumns}
                 pageSizeOptions={[10, 25, 50, 100, 250, 500, 1000, 5000, 10000]}
                 initialState={{
@@ -19,6 +41,7 @@ const TicketQueueTable = () => {
                         },
                     },
                 }}
+                loading={loading}
             />
         </div>
     );
