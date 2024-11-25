2a9e06bd285fd7dc82f285bb35c4e843c0531b0c
Ahsan Tahir <atahir@visp.net>
2024-11-25 19:07:27 +0500

    [CRM-813] Task > Remove * from the tasks those were added by a user and not from ticket template settings

--- src/TicketDetails/components/Tasks/index.js
+++ src/TicketDetails/components/Tasks/index.js
@@ -364,7 +364,7 @@ const Tasks = (props) => {
                                         {`${task.task}${" *"}`}
                                       </span>
                                     ) : (
-                                      `${task.task}${isTaskRequired ? " *" : ""}`
+                                      `${task.task}`
                                     )}
                                   </Typography>
                                 }
